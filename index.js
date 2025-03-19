9b57ad82247092457ae50d0b0beee1aafdc30c34
Ryan James <ryanrjames24@gmail.com>
2025-03-19 00:36:37 +0000

    Functioning buttons
    apart from the 'start new game'

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 let homeScoreBtnOne = document.getElementById("home-score-btn-1")
-let homeStoreEl =document.getElementById("home-score")
+let homeStoreEl = document.getElementById("home-score")
 let homeScore = 0
 
 function increaseHomeScoreOne(){
     homeScore += 1
     homeStoreEl.textContent = homeScore
+}
+
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
+
+function increaseHomeScoreTwo(){
+    homeScore += 2
+    homeStoreEl.textContent = homeScore
+}
+
+let homeScoreBtnThree = document.getElementById("home-score-btn-3")
+
+function increaseHomeScoreThree(){
+    homeScore += 3
+    homeStoreEl.textContent = homeScore
+}
+
+let awayScoreBtnOne = document.getElementById("away-score-btn-1")
+let awayStoreEl = document.getElementById("away-score")
+let awayScore = 0
+
+function increaseAwayScoreOne() {
+    awayScore += 1
+    awayStoreEl.textContent = awayScore
+}
+
+let awayScoreBtnTwo = document.getElementById("away-score-btn-2")
+
+function increaseAwayScoreTwo() {
+    awayScore += 2
+    awayStoreEl.textContent = awayScore
+}
+
+let awayScoreBtntThree = document.getElementById("away-score-btn-3")
+
+function increaseAwayScoreThree() {
+    awayScore += 3
+    awayStoreEl.textContent = awayScore
 }
